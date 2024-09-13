3c6ab9964e9af3b53f5f56efd06f0ccbbb3d13b6
Olumide <olumidebamgboje@gmail.com>
2024-09-13 15:10:35 +0100

    home screen and about us screen added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -24,7 +24,7 @@ export const Footer = () => {
             are responsible for ensuring that their activities comply with applicable laws.
             </p>
 
-              <div className="absolute ml-5.4 mt-6 sm:mt-6 sm:ml-5 flex flex-col gap-3 lg:gap-3 text-left lg:mt-7 lg:ml-8">
+              <div className="absolute ml-5 mt-6 sm:mt-6 sm:ml-5 flex flex-col gap-3 lg:gap-3 text-left lg:mt-7 lg:ml-8">
               <h3 className="relative text-white font-bold lg:text-sm lg:hover:text-green">Company</h3>
               <Link to="/aboutus"><h3 className="relative text-white lg:text-sm lg:hover:text-green">About Us</h3></Link>
               <h3 className="relative text-white-light lg:text-sm lg:hover:text-green">Contact</h3>
@@ -37,7 +37,7 @@ export const Footer = () => {
               <p className="absolute mt-18 ml-7.5 text-base text-white-light text-left max-w-22">support@pickbetta.com</p>
               </div>
 
-              <img src={line} alt="" className="absolute h-0.1 mt-19.3 ml-5 sm:h-0.1 sm:mt-19.3 sm:ml-5 lg:mt-19.2 lg:ml-39 w-19.5" />
+              <img src={line} alt="" className="absolute h-0.1 mt-19.3 ml-5 sm:h-0.1 sm:mt-19.3 sm:ml-5 lg:mt-19.2 lg:ml-39 w-20.2" />
 
               <div className="absolute mt-19.6 ml-12 sm:mt-19.6 sm:ml-15 lg:mt-19.6 lg:ml-39 flex gap-2">
               <img src={facebook} alt="for facebook link" className="relative h-4.5" />
